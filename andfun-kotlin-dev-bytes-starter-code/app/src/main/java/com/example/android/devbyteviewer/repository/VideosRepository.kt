35ba1ff445c38b5a1307d1b9de40a91ceb863a90
patrick <patrickchow0803@gmail.com>
2019-12-28 09:46:06 -0500

    Added a refreshVideos function

--- andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,50 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/*
+ * Repository for fetching devbyte videos from the network and storing them on disk.
+ */
+class VideosRepository (private val database: VideosDatabase){
+
+    suspend fun refreshVideos(){
+        // Reading and writing from a disk is called disk IO. Therefore use Dispatchers.IO
+        // withContext forces a Kotlin Coroutine to switch to the dispatchers specified.
+        withContext(Dispatchers.IO){
+
+            // await() to tell the coroutine to suspend until it's available
+            val playlist = Network.devbytes.getPlaylist().await()
+            database.videoDao.insertAll(*playlist.asDatabaseModel())
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
